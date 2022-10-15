16795922398b51dce6a5269d05b434020d1a263e
OlgaShalaginova <olechkatsapova@gmail.com>
2022-10-15 19:06:03 +0500

    Task34

--- task34/Program.cs
+++ task34/Program.cs
@@ -5,32 +5,32 @@ Console.WriteLine("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("Массив: ");
 PrintArray(numbers);
 FindChetEl(numbers);
 
-void FindChetEl (int [] numbers)
-{
- int count = 0;
- for (int x = 0; x < numbers.Length; x++)
- if (numbers[x] % 2 == 0)
- count++;
- Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них чётные");
-}
 void FillArrayRandomNumbers(int[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    for(int i = 0; i < size; i++)
     {
         numbers[i] = new Random().Next(100,1000);
     }
 }
 void PrintArray(int[] numbers)
 {
+    Console.WriteLine("Массив: ");
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    for(int i = 0; i < size; i++)
     {
         Console.Write(numbers[i] + " ");
     }
     Console.Write("]");
     Console.WriteLine();
+}
+void FindChetEl (int [] numbers)
+{
+ int count = 0;
+ for (int x = 0; x < size; x++)
+ if (numbers[x] % 2 == 0)
+ count++;
+ Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них чётные");
 }
