db8e1f58b6ceaace3f75f7a0a1af6a54ef30f845
OlgaShalaginova <olechkatsapova@gmail.com>
2022-10-15 19:11:30 +0500

    Task38

--- task38/Program.cs
+++ task38/Program.cs
@@ -5,15 +5,32 @@ Console.WriteLine("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 double[] numbers = new double[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("Массив: ");
 PrintArray(numbers);
 Raznica(numbers);
 
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < size; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.WriteLine("Массив: ");
+    Console.Write("[ ");
+    for(int i = 0; i < size; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
 void Raznica(double[] numbers)
 {
  double minValue = Int32.MaxValue;
  double maxValue = Int32.MinValue;
- for (int x = 0; x < numbers.Length; x++)
+ for (int x = 0; x < size; x++)
   {
     if (numbers[x] > maxValue)
         {
@@ -24,23 +41,6 @@ void Raznica(double[] numbers)
             minValue = numbers[x];
         }
  }
- Console.WriteLine($"Всего {numbers.Length} чисел. Максимальное значение = {maxValue}, минимальное значение = {minValue}");
+ Console.WriteLine($"Всего {size} чисел. Максимальное значение = {maxValue}, минимальное значение = {minValue}");
  Console.WriteLine($"Разница между максимальным и минимальным значением = {maxValue - minValue}");
-}
-void FillArrayRandomNumbers(double[] numbers)
-{
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
-        }
-}
-void PrintArray(double[] numbers)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
-    Console.Write("]");
-    Console.WriteLine();
 }
