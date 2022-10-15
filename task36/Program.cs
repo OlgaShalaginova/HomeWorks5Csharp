b1c630a2e1cdf0248d9f65c95120dcea0ecb2235
OlgaShalaginova <olechkatsapova@gmail.com>
2022-10-15 19:08:38 +0500

    Task36

--- task36/Program.cs
+++ task36/Program.cs
@@ -5,31 +5,31 @@ Console.WriteLine("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("Массив: ");
 PrintArray(numbers);
 SumElArray(numbers);
 
-void SumElArray (int [] numbers)
- {
- int sum = 0;
- for (int x = 0; x < numbers.Length; x+=2)
-    sum = sum + numbers[x];
-    Console.WriteLine($"Всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
- }
 void FillArrayRandomNumbers(int[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    for(int i = 0; i < size; i++)
         {
             numbers[i] = new Random().Next(1,10);
         }
 }
 void PrintArray(int[] numbers)
 {
+    Console.WriteLine("Массив: ");
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    for(int i = 0; i < size; i++)
         {
             Console.Write(numbers[i] + " ");
         }
     Console.Write("]");
     Console.WriteLine();
 }
+void SumElArray (int [] numbers)
+ {
+ int sum = 0;
+ for (int x = 0; x < size; x+=2)
+    sum = sum + numbers[x];
+    Console.WriteLine($"Всего {size} чисел, сумма элементов на нечётных позициях = {sum}");
+ }
